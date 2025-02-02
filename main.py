5dab3e899c5996f9dc39b1acf76772ddf1d6c152
hungryakira <jon.kh@hotmail.com>
2025-02-02 15:58:59 +0800

    Update main.py
    Editing for comments

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
 
 app = Flask(__name__)
 
+#Retrieve Blog text from api
 response = requests.get(blog_url)
 all_posts = response.json()
 
+#---- Home Page showing list of blog posts using template "index.html"
 @app.route('/')
 def home():
     return render_template("index.html", posts = all_posts)
 
+#---- create pages for individual pages using the template "post.html"
 @app.route('/post/<int:id>')
 def post(id):
     for entry in all_posts:
